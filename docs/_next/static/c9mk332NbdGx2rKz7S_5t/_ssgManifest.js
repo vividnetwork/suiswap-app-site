aaaf795f4a9ae7d5133b3d2840dffd7282d5573b
autobuild <vivid.network@outlook.com>
2023-04-25 02:27:30 +0800

    Auto build from commit df429ae

--- docs/_next/static/c9mk332NbdGx2rKz7S_5t/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fdebug\u002Fbuild_info"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
---
+++ docs/doc/doc-layout.ts
@@ -0,0 +1,48 @@
+import { DocLayout } from "./doc-layout-type-definition"
+export * from "./doc-layout-type-definition"
+
+
+export const docLayout: DocLayout = [
+    {
+        category: "suiswap",
+        categoryShow: "SUISWAP",
+        content: [
+            {
+                section: "suiswap",
+                sectionShow: "Suiswap",
+                articles: [
+                    {
+                        "time": "2023-04-24",
+                        "name": "introduction-to-suiswap",
+                        "nameShow": "💡· Introduction To Suiswap",
+                        "description": "Suiswap is a decentralized token trading platform and exchange built on the SUI blockchain by Vivid Network. It aims to provide a secure, fast, and agile trading environment for the SUI ecosystem.",
+                        "markdownPath": "/doc/articles/introduction-to-suiswap.md",
+                        "cover": "/doc/cover/suiswap-doc-image.jpeg",
+                        "thumbnailImage": "/doc/cover/suiswap-doc-image.jpeg"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        category: "community",
+        categoryShow: "COMMUNITY",
+        content: [
+            {
+                section: "community",
+                sectionShow: "Community",
+                articles: [
+                    {
+                        "time": "2023-04-24",
+                        "name": "link-your-wallet-address",
+                        "nameShow": "💰· Link Your Wallet Address",
+                        "description": "Suiswap is a decentralized token trading platform and exchange built on the SUI blockchain by Vivid Network. It aims to provide a secure, fast, and agile trading environment for the SUI ecosystem.",
+                        "markdownPath": "/doc/articles/link-your-wallet-address.md",
+                        "cover": "/doc/cover/link-your-wallet-cover.jpg",
+                        "thumbnailImage": "/doc/cover/link-your-wallet-cover.jpg",
+                    }
+                ]
+            }
+        ]
+    }
+]
