6493c5878b1b1c35a47e8b44b8db1f417ea9c12e
autobuild <vivid.network@outlook.com>
2024-02-05 16:57:08 +0200

    Auto build from commit 7523e97

---
+++ docs/_next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/404":["static\u002Fchunks\u002Fpages\u002F404.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/staking":["static\u002Fchunks\u002Fpages\u002Fstaking.js"],sortedPages:["\u002F404","\u002F_app","\u002F_error","\u002Fstaking"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
